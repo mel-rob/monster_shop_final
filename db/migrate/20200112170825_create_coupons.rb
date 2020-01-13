74eb6d37f4c7832e24e41c04a60e523fb36a3b82
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-12 18:58:56 -0700

    Remove active field from Coupons table

--- db/migrate/20200112170825_create_coupons.rb
+++ db/migrate/20200112170825_create_coupons.rb
@@ -4,7 +4,6 @@ class CreateCoupons < ActiveRecord::Migration[5.1]
       t.string :name
       t.decimal :percentage_off
       t.string :code
-      t.boolean :active?, default: true
 
       t.references :merchant, foreign_key: true
 
