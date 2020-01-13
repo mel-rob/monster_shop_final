a25fab6c654fe6bd1656ced10610b1700d1f1cf1
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-12 21:17:31 -0700

    Add tests for delete button conditional

--- spec/features/merchant/coupons/show_spec.rb
+++ spec/features/merchant/coupons/show_spec.rb
@@ -8,7 +8,18 @@ RSpec.describe 'Coupons show page' do
       @coupon_1 = @merchant_1.coupons.create!(name: 'Black Friday', percentage_off: 20, code: 'BF2019')
       @coupon_2 = @merchant_1.coupons.create!(name: 'Mega Sale', percentage_off: 10, code: 'mega10')
 
+      @default_user = User.create!(name: 'Melissa D.', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'melissad@gmail.com', password: 'password')
+
       @user_1 = @merchant_1.users.create!(name: 'Melissa R.', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'melissar@gmail.com', password: 'password')
+
+      @order_item_1 = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @order_item_2 = @merchant_1.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
+
+      @order_1 = @default_user.orders.create!
+      @order_item_1 = @order_1.order_items.create!(item: @order_item_1, price: @order_item_1.price, quantity: 2)
+      @order_item_2 = @order_1.order_items.create!(item: @order_item_2, price: @order_item_2.price, quantity: 3)
+
+      @coupon_2.orders << @order_1
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
     end
@@ -59,6 +70,16 @@ RSpec.describe 'Coupons show page' do
       expect(page).to_not have_content(@coupon_1.name)
       expect(page).to_not have_content(number_to_percentage(@coupon_1.percentage_off, precision: 0))
       expect(page).to_not have_content(@coupon_1.code)
+    end
+
+    it "I don't see delete buttons on coupons that have already been applied" do
+
+      visit merchant_coupon_path(@coupon_2)
+
+      expect(page).to_not have_button('Delete')
+      expect(page).to have_content('This coupon has been applied and cannot be deleted.')
+
+      save_and_open_page
     end
   end
 end
