bd418f43855c8dca6c7a34616fb462aab0c57a96
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-13 18:35:58 -0700

    Add tests to cart show for coupons

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'Cart Show Page' do
     before :each do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+
+      @coupon_1 = @megan.coupons.create!(name: 'Black Friday', percentage_off: 20, code: 'BF2019')
+      @coupon_2 = @megan.coupons.create!(name: 'Mega Sale', percentage_off: 10, code: 'Mega10')
+
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
@@ -167,6 +171,52 @@ RSpec.describe 'Cart Show Page' do
         expect(page).to_not have_content("#{@hippo.name}")
         expect(page).to have_content("Cart: 0")
       end
+    end
+
+    it 'I see a field to add a coupon code' do
+      visit item_path(@ogre)
+      click_button 'Add to Cart'
+      visit item_path(@hippo)
+      click_button 'Add to Cart'
+      visit item_path(@hippo)
+      click_button 'Add to Cart'
+
+      visit '/cart'
+
+      expect(page).to have_content('Coupon code')
+      find_field(:coupon_code)
+      expect(page).to have_button('Submit')
+    end
+
+    it 'I can enter in a coupon code and apply it to my cart' do
+      visit item_path(@ogre)
+      click_button 'Add to Cart'
+      visit item_path(@hippo)
+      click_button 'Add to Cart'
+      visit item_path(@hippo)
+      click_button 'Add to Cart'
+
+      visit '/cart'
+
+      fill_in :coupon_code, with: 'BF2019'
+      click_button 'Submit'
+
+      expect(page).to have_content('BF2019 coupon applied!')
+      expect(page).to have_content('BF2019 coupon currently applied.')
+
+      fill_in :coupon_code, with: 'Mega10'
+      click_button 'Submit'
+
+      expect(page).to have_content('Mega10 coupon applied!')
+      expect(page).to have_content('Mega10 coupon currently applied.')
+
+      expect(page).to_not have_content('BF2019 coupon currently applied.')
+
+      fill_in :coupon_code, with: 'LameCoupon'
+      click_button 'Submit'
+
+      expect(page).to have_content('This coupon does not exist or is not valid for these items.')
+      expect(page).to have_content('Mega10 coupon currently applied.')
     end
   end
 end
